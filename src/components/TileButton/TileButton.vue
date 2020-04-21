ad024e6eb882ee2db09690567f047773521f4b86
ZerdoX-x <zerdox.cool4@gmail.com>
2020-04-21 16:38:38 +0500

    Tile Button added

--- src/components/TileButton/TileButton.vue
+++ src/components/TileButton/TileButton.vue
@@ -1,11 +1,45 @@
 <template>
-  <button>Join</button>
+  <button class="tile-button">
+    <span tabindex="-1" class="tile-button__content">
+      Join
+    </span>
+  </button>
 </template>
 
-<script>
-export default {
-  name: "TileButton"
-};
-</script>
+<style lang="stylus" scoped>
+.tile-button
+.tile-button__content
+  transition background-color 0.3s, color 0.3s
+  display flex
+.tile-button
+  cursor pointer
+  background-color #005be9
+  border none
+  font-family "Open Sans"
+  color #FFFFFF
+  text-transform uppercase
+  font-weight bold
+  max-width 280px
+  width 87.5vw
+  min-width 260px
+  height 64px
+  margin 0 auto
+  align-self center
+  padding 0
+  &:hover
+    background-color #166efa
+  &:focus > .tile-button__content
+    outline none
+    background-color #166efa
+    color #95ff00
 
-<style lang="stylus" scoped></style>
+.tile-button__content
+  width 100%
+  height 100%
+  align-items center
+  justify-content center
+
+.tile-button:focus
+.tile-button__content:focus
+  outline none
+</style>
